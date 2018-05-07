40ee756cb2b94bd420a837d49fddc0e33bfaad2b
MemoryTime <39057589+MemoryTime@users.noreply.github.com>
2018-05-07 21:18:54 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = date("Y/m/d");
+			$text = $event['source'][date("Y/m/d")];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
