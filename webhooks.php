c6d5ccf83834bd4999f974708732215c532b1a7a
MemoryTime <39057589+MemoryTime@users.noreply.github.com>
2018-11-19 21:17:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,9 +16,9 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = 'หัวข้อนัดหมาย: รายงานผลตรวจร่างกาย \n'.
-			'ชื่อสัตว์เลี้ยง: มะยม \n'.
-			'ชื่อสัตวแพทย์: บิ๊ก \n'.
+			$text = 'หัวข้อนัดหมาย: รายงานผลตรวจร่างกาย'.'\n'.
+			'ชื่อสัตว์เลี้ยง: มะยม'.'\n'.
+			'ชื่อสัตวแพทย์: บิ๊ก'.'\n'.
 			'กำหนดนัดหมาย: วันที่ 28 พฤศจิกายน 2561 เวลา 17:00-18:00 น.';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
