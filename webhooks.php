177f18bb93d7a48b86a14d5e2eddfffe954303c1
MemoryTime <39057589+MemoryTime@users.noreply.github.com>
2018-12-25 21:59:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,10 +16,10 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = 'test';
+			$text = $event['source']['userId'];
 			/*$text = "หัวข้อนัดหมาย: \nรายงานผลตรวจร่างกาย\n\nชื่อสัตว์เลี้ยง: \nมะยม\n\nชื่อสัตวแพทย์: \nบิ๊ก\n\nกำหนดการนัดหมาย: \nวันที่ 28 พฤศจิกายน 2561\nเวลา 17:00-18:00 น.";*/
 			// Get replyToken
-			$replyToken = 'Uffa6b3fef1c10f8eaaf947188a01b687';
+			$replyToken = "Uffa6b3fef1c10f8eaaf947188a01b687";
 
 			// Build message to reply back
 			$messages = [
@@ -49,33 +49,4 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-$text = 'test';
-			/*$text = "หัวข้อนัดหมาย: \nรายงานผลตรวจร่างกาย\n\nชื่อสัตว์เลี้ยง: \nมะยม\n\nชื่อสัตวแพทย์: \nบิ๊ก\n\nกำหนดการนัดหมาย: \nวันที่ 28 พฤศจิกายน 2561\nเวลา 17:00-18:00 น.";*/
-			// Get replyToken
-			$replyToken = 'Uffa6b3fef1c10f8eaaf947188a01b687';
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
+echo "OK";
