25d0a7022e6794eb99e04861db97b1f699f46b5c
MemoryTime <39057589+MemoryTime@users.noreply.github.com>
2018-12-25 22:11:15 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = '5Qd9Y/veRs1+G6AGs9m7mAs+LJm3AHGMEIzLzxJ1MoLx8zIvDjAnzFTV5NTpaLFY/pZR3CgI1dBecnCXjvK+e4Ihrpa0EFncTFE7VEae0h/1SlWqYDG50ZRCNdoMYywS0ioxXo/1I4PJTe3wGtR+fgdB04t89/1O/w1cDnyilFU=';
 
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
